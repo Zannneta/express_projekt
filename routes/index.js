60018cc83c7528fe7557ddc1cac4008d678fcb3c
Zannneta <zaneta.komos@gmail.com>
2021-04-04 00:45:29 +0200

    first

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Świat kawy'});
+});
+router.get('/oferty', function (req, res, next) {
+  res.render('oferty', {title: 'Oferty specjalne'});
+});
+router.get('/ekspresy', function (req, res, next) {
+  res.render('ekspresy', {title: 'Ekspresy', my_var: req.params.ekspres, s: ''});
+});
+router.post('/ekspresy', (req, res) => {
+  let searchString = ""
+  if (req.body.search) {
+    searchString = req.body.search.toLowerCase();
+  }
+  res.render('ekspresy', {title: 'Ekspresy', my_var: req.params.ekspres, s: searchString});
+});
+router.get('/uslugi', function (req, res, next) {
+  res.render('uslugi', {title: 'Nasze usługi', error: false})
+});
+router.post('/uslugi', (req, res) => {
+  const fs = require('fs');
+  const path = require('path');
+
+  console.log(req.body.file)
+  try {
+    let pathe = path.join(__dirname, '../public/pdf/' + req.body.file + '.pdf');
+    if (fs.existsSync(pathe)) {
+      res.download(pathe)
+    } else {
+      res.render('uslugi', {title: 'Nasze usługi', error: true})
+    }
+  } catch (err) {
+    console.error(err)
+  }
+});
+
+router.get('/kontakt', function (req, res, next) {
+  res.render('kontakt', {title: 'Zapraszamy do kontaktu'});
+});
+
+module.exports = router;
